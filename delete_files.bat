ba245a3c1d59c1a46383702e3897625671fbc5f1
Felix Tischler <felix.tischler@uni-jena.de>
2022-11-24 12:00:31 +0100

    remove ignored files with batch skript

---
+++ delete_files.bat
@@ -0,0 +1,19 @@
+del \*.aux
+@echo off
+CD C:\Users\flxti\Dropbox\UNI\AUB_2022_23\AUB_2022_23 "root folder"
+for /F "delims=" %%G in ('dir /b') do (
+    REM check if it is a directory or file
+    IF EXIST "%%G\" (
+        cd "%%G"
+        del *.aux
+        del *.log
+        del *.gz
+        del *.gout
+        cd ..
+    ) else (
+        del *.aux
+        del *.log
+        del *.gz
+        del *.gout
+    )
+)
