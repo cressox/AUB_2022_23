bbd201fada12f26b3a026c8dafacacc896751e86
Felix Tischler <felix.tischler@uni-jena.de>
2022-11-28 11:22:06 +0100

    u5

--- U4/t1_g.py
+++ U4/t1_g.py
@@ -1,4 +1,6 @@
 for i in range(100):
-	if i%3==0:
-		tmp = ['0' for i in range(8-len(bin(i)[2:]))]
-		print(f"{''.join(tmp)}{bin(i)[2:]}\t{i}")
+	if str(bin(i)).count("1")%3==0:
+		if i%3==0:
+			print(bin(i)[2:], i)
+
+		
